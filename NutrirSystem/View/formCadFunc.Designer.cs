ac92cd8345a2a3a5362623339afe500529c9cfa9
Lucas Carvalho Corrêa <lucasccorrea@gmail.com>
2014-09-01 19:08:43 -0300

    Inicio da criação FormsCadFunc
    Comecei a criar o formCadFunc

---
+++ NutrirSystem/View/formCadFunc.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace View
+{
+    partial class formCadFunc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxCpf = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.boxNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // boxCpf
+            // 
+            this.boxCpf.Location = new System.Drawing.Point(12, 87);
+            this.boxCpf.Name = "boxCpf";
+            this.boxCpf.Size = new System.Drawing.Size(163, 20);
+            this.boxCpf.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPF";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // boxNome
+            // 
+            this.boxNome.Location = new System.Drawing.Point(12, 150);
+            this.boxNome.Name = "boxNome";
+            this.boxNome.Size = new System.Drawing.Size(163, 20);
+            this.boxNome.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome";
+            // 
+            // formCadFunc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 343);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.boxNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boxCpf);
+            this.Name = "formCadFunc";
+            this.Text = "formCadFunc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox boxCpf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox boxNome;
+        private System.Windows.Forms.Label label2;
+    }
+}
